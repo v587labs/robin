63ee4d536e1c3912e75dc8f23ab8e98abef82611
liwu <liwuv587@gmail.com>
2024-01-03 14:57:29 +0800

    model log

--- gflog/log.go
+++ gflog/log.go
@@ -2,13 +2,18 @@ package gflog
 
 import (
 	"context"
+	"slices"
+
 	"github.com/gogf/gf/v2/container/gmap"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/glog"
-	"slices"
 )
 
 type _contextKey struct {
+}
+
+type Logger struct {
+	*glog.Logger
 }
 
 const LogCtxKey = "vars"
@@ -17,11 +22,15 @@ func Log(name ...string) *glog.Logger {
 	l := g.Log(name...)
 	if !slices.Contains(l.GetCtxKeys(), LogCtxKey) {
 		l.AppendCtxKeys(LogCtxKey)
+		l.SetHandlers(func(ctx context.Context, in *glog.HandlerInput) {
+			in.Next(ctx)
+		})
 	}
 	return l
 }
 
 func ContextValue(ctx context.Context, key string, val any) context.Context {
+	// todo fix this
 	value := ctx.Value(LogCtxKey)
 	gm, ok := value.(gmap.Map)
 	if !ok {
